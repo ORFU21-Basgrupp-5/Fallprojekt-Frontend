3c76fc1e4b9cdb5f7e910fcbd33fdb2cfd1f8563
MikaelaNystrom <mikaela.hedstrom@gmail.com>
2022-01-28 13:10:37 +0100

    Knappar funkar

--- Inmatning/script.js
+++ Inmatning/script.js
@@ -1,8 +1,33 @@
-const Expense = (C) =>
-{
 
-}
-const Income = (C) =>
-{
+let Exp = document.getElementById("utgifter")
+let Inc = document.getElementById("Inkomster")
+let Expbutton = document.getElementById("expense")
+let Incbutton = document.getElementById("income")
 
+// const Expense = (C)=>
+// {
+//     console.log("Du lade till en utgift")
+// }
+// const Income = (C)=>
+// {
+//     console.log("Du lade till en inkomst")
+// }
+
+Expbutton.onclick = (e) =>
+{
+    e.preventDefault()
+    console.log("Du lade till en utgift")
 }
+
+Incbutton.onclick = (i) =>
+{
+    i.preventDefault()
+    console.log("Du lade till en inkomst")
+    
+}
+
+
+// }
+// Expense()
+// Income()
+// // debugger
