471376e07310607ee0e71c28eeb85924476e2212
Daniel Kjellberg <danielkjellberg@hotmail.com>
2022-02-02 14:30:17 +0100

    Hämta data från databas

--- Lista/script.js
+++ Lista/script.js
@@ -1,16 +1,36 @@
 let ListExp = document.getElementById("ListaUtgifter")
 
-let data = ['Mat', 'Nöje', 'Hyra', 'Bilutgifter' ];
-  
-let list = document.getElementById("ListaUtgifter");
-let DivWithExpenses = document.getElementById("DivWithExpenses")
-
 ListExp.onclick = (e) =>
 {
-data.forEach((item)=>{
-  let li = document.createElement("li");
-  li.innerText = item;
-  DivWithExpenses.appendChild(li);
-  ListExp.remove()
-})
+  let utgifter = [];
+  fetch("https://localhost:7151/ListExpenses", {
+    method: "GET", 
+    headers: {
+      'Content-Type': 'application/json'
+    }})
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    utgifter = data;
+    upgiftsLista(utgifter)
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+}
+
+function upgiftsLista(data) {
+  data.forEach((item)=>{
+
+    for (let row in item) {
+      let li = document.createElement("li");
+      li.innerText = `${row}: ${item[row]}`;
+      DivWithExpenses.appendChild(li);
+      ListExp.remove()
+    }
+  })
 }
