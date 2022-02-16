af25fb3f3933e293c0e80b596372a55016557523
KimNordstrom <Kim.nordstrom@outlook.com>
2022-02-16 13:34:31 +0100

    Changed & fixed some names

--- inmatning.js
+++ inmatning.js
@@ -3,7 +3,7 @@ export const render = (root) => {
   // var stringUtgifter = '<div><form id="Utgifter"><div><p>Inmatning av utgifter</p></div><div id="info-utgift"></div><div><label for = "Saldo"> Utgift, saldo:</label></div> <div>   <input type = "text" id= "Saldo" name= saldo></div><div><label for="Konto">Utgift, konto:</label></div> <div><input type="text" id= "Konto" name= konto></div><div><label for="Description">Utgift, beskrivning:</label></div><div>  <input type="text" id= "Description" name= description></div><div> <label for="Date">Utgift, datum:</label></div> <div><input type="date" id= "Date" name= date></div><div><button id= "expense">Enter</button></div</form></div>'
   // var stringInkomster = '<div><form id ="Inkomster"><div><p>Inmatning av Inkomster</p></div><div id="info-inkomst"></div><div><label for = "Saldo"> Inkomst, saldo:</label></div><div><input type = "text" id= "Saldo" name= saldo></div><div><label for="Konto">Inkomst, konto:</label></div><div><input type="text" id= "Konto" name= konto></div><div><label for="Description">Inkomst, beskrivning:</label></div><div><input type="text" id= "Description" name= description></div><div><label for="Date">Utgift, datum:</label></div><div> <input type="date" id= "Date" name= date> </div><div><button id= "income">Enter</button></div></form></div>'
   
-  let elem = [
+  let elementStrings = [
     "label",
     "input",
     "label",
@@ -15,7 +15,7 @@ export const render = (root) => {
     "button"
   ]
 
-  let attri = [
+  let attributes = [
     "for", "Saldo",
     "id", "Saldo",
     "for", "Konto",
@@ -61,8 +61,8 @@ export const render = (root) => {
   pageContent.appendChild(divutgift)
   pageContent.appendChild(ExpenseForm)
 
-  SetAttribuites(elem, attri, "I", IncomeForm)
-  SetAttribuites(elem, attri, "E", ExpenseForm)
+  SetAttributes(elementStrings, attributes, "I", IncomeForm)
+  SetAttributes(elementStrings, attributes, "E", ExpenseForm)
 
   let IncSubmit = document.getElementById("ISubmit")
   let ExpSubmit = document.getElementById("ESubmit")
@@ -102,7 +102,7 @@ const expense = (e) => {
   }
 }
 
-function SetAttribuites(arr, arr2, letter, form){
+function SetAttributes(arr, arr2, letter, form){
   let attnum = 0;
   let elements = []
   for (let index = 0; index < arr.length; index++) {
