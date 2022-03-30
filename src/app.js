c2abbd684d4d0bb6f3b988b3b6222edf7bd4c862
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-03-30 17:17:31 +0200

    added comments

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ import { removeCookies } from "/services/cookie.js";
 import { defaultRender } from "/services/errorHandler.js";
 import API_Service from '/services/API_Service.js';
 
-
+//the routing adresses and what page they are assoiated with
 const routes = {
     '/'                 : Home
     , '/budget'         : Budget
@@ -41,42 +41,46 @@ const routes = {
 };
 
 const router = async () => {
-
+    
+    //gets header etc once only
     const header = null || document.getElementById('headerContent');
     const content = null || document.getElementById('pageContent');
     const footer = null || document.getElementById('footerContent');
 
   
-
+    //checks the current adress route and moves to the correct page
     let request = routerService.parseRequestURL()
     console.log(request);
     let parsedURL = (request.resource ? '/' + request.resource : '/') + (request.id ? '/:id' : '') + (request.verb ? '/' + request.verb : '')
     let page;
     if(parsedURL === '/logout')
     {
+        //if route is logout we remove cookie and displa godbyepage.
         removeCookies();
         page = logout       
     }
     else
     page = routes[parsedURL] ? routes[parsedURL] : Error404
 
-
+    //if  user is logged in we send "loged in menu to header"
     var userString = getCookie('user');
     var userLogedIn = false;
     if(userString != null) 
     userLogedIn = true;
 
+    //if user is not logged in we send only login and register to menu
     header.innerHTML = await NavigationBar.render(userLogedIn);
     await NavigationBar.after_render();
     footer.innerHTML = await Footer.render();
     await Footer.after_render();
 
-
+    //send the correct page + functions that the routingservice gives us
     content.innerHTML = await page.render();
     await page.after_render();
 
 }
-
+//The hashchange event is fired when the fragment identifier of the URL has changed (the part of the URL beginning with and following the # symbol).
 window.addEventListener('hashchange', router);
 
+//event when page is fully loaded
 window.addEventListener('load', router);
