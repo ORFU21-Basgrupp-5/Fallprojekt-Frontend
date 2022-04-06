10d5a69ab137bc69bf3e8a08d9473939d829dab6
Adam Lord Eriksson <90420563+A-Lord@users.noreply.github.com>
2022-04-06 14:11:42 +0200

    Update header.js
    added logout rout to main page no mather what adress its hosted from

--- header.js
+++ header.js
@@ -39,7 +39,7 @@ export class Header {
     });
     button6.addEventListener("click", () => {
       deleteCookie("token");
-      window.location.href = "http://127.0.0.1:5500/";
+      window.location.href = "/";
     });
     
 
